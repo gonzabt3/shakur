59a55d72ff71c21cd7bada63217d5323408b945a
gonza <gonzalo.muscolo@gmail.com>
2018-08-11 20:41:26 -0300

    agrego modal para nuevo eventos

---
+++ src/components/modals/ModalNewEvent.vue
@@ -0,0 +1,26 @@
+<template>
+     <b-modal id="newEvent" title="Nuevo evento">
+        <b-container>
+            <b-form>
+                <b-form-group label="Nombre:" label-for="nombre">
+                    <b-form-input id="nombre"
+                                required
+                                placeholder="Ingresa tu nombre">
+                    </b-form-input>
+                </b-form-group>
+            </b-form>
+        </b-container>
+        <template slot="modal-footer">
+      <button class="btn btn-success btn-block" type="submit">Crear</button>
+    </template>
+    </b-modal>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
+<style  scoped>
+
+</style>
