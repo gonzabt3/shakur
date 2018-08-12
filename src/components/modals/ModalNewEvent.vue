52ed3d9bfd323b22310a5f262e045a478af80f47
gonza <gonzalo.muscolo@gmail.com>
2018-08-12 17:01:08 -0300

    agrego calendar al modal

--- src/components/modals/ModalNewEvent.vue
+++ src/components/modals/ModalNewEvent.vue
@@ -4,9 +4,16 @@
             <b-form>
                 <b-form-group label="Nombre:" label-for="nombre">
                     <b-form-input id="nombre"
-                                required
-                                placeholder="Ingresa tu nombre">
+                                placeholder="Ingresa nombre del evento">
                     </b-form-input>
+                </b-form-group>
+                <b-form-group label="Temas:" label-for="temas">
+                    <b-form-input id="temas"
+                                placeholder="Ingresa los temas del parcial">
+                    </b-form-input>
+                </b-form-group>
+                <b-form-group label="Seleccionar Fecha">
+                    <datepicker class="form-control" :language="es" placeholder="Seleccionar fecha"></datepicker>
                 </b-form-group>
             </b-form>
         </b-container>
@@ -16,9 +23,17 @@
     </b-modal>
 </template>
 <script>
+import Datepicker from 'vuejs-datepicker';
+import {en, es} from 'vuejs-datepicker/dist/locale'
 
 export default {
-    
+    components: {Datepicker},
+    data(){
+        return {
+            en: en,
+            es: es
+        }
+    }
 }
 </script>
 <style  scoped>
