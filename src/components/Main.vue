7df4cf488d50dce003317e4a52fa9040553e4652
gonza <gonzalo.muscolo@gmail.com>
2018-08-22 12:55:32 -0300

    empiezo a abstraer componentes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,7 +7,7 @@
             </b-col>
             <b-col cols="5">
                 <post-new></post-new>
-                <post-user></post-user>
+                <post-user v-bind:postData="objectPost"></post-user>
             </b-col>
             <b-col>
                 <events-wall class="form-group"></events-wall>
@@ -29,6 +29,17 @@ import PostNew from '@/components/PostNew';
 export default {
   name: 'Main',
   components: { Topbar, PostUser, EventsWall, DocWall, SettingsWall, PostNew },
+  data(){
+      return{
+          objectPost:{ 
+            name: 'Pepe San martin',
+            comentario:'la concha del pato',
+            fecha:'20 de agosto 2055',
+            imagen:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Jos%C3%A9_de_San_Mart%C3%ADn_%28retrato%2C_c.1828%29.jpg/457px-Jos%C3%A9_de_San_Mart%C3%ADn_%28retrato%2C_c.1828%29.jpg',
+            likes:7 
+            }
+      }
+  }
 };
 </script>
 
