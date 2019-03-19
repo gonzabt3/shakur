75d692e3a60e0a9d0f7598613a3677468f3a3c03
gonzabt3 <fm_gonza@hotmail.com>
2019-03-19 16:48:09 -0300

    agrego el 404 mas choto del mundo

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Shakur - 404</title>
+    </head>
+    <body background="images/404.jpg" style="  background-size: 100% 100%;
+background-color:black;  background-position: center;  background-attachment: fixed; background-repeat:no-repeat;">
+    </body>
+</html>
