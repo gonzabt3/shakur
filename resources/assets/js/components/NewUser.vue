60db89888e75b77dd1169dcb15e58db7811500f3
gonzabt3 <fm_gonza@hotmail.com>
2019-03-28 13:56:51 -0300

    agrego validaciones en el newUser

--- resources/assets/js/components/NewUser.vue
+++ resources/assets/js/components/NewUser.vue
@@ -1,45 +1,63 @@
 <template>
     <b-modal @hide="cleanModal" id="newUser" ref="newUser" title="Crear Usuario">
         <b-container>
-            <b-form>
-                <b-form-group  label="Nombre:" label-for="name" :class="{'has-error': errors.has('name')}" >
+            <b-form >
+                <b-form-group  label="Nombre:" label-for="name"  >
                     <b-form-input id="name"
                                 name="name"
                                 v-model="usuario.name"
                                 required
+                                :class="{'is-invalid': errors.has('name')}"
                                 placeholder="Ingresa tu nombre"
                                 v-validate="'required'"
                                 >
                     </b-form-input>
-                        <p class="text-danger" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+                    <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group label="Apellido:" label-for="apellido">
                     <b-form-input id="apellido"
+                                name="apellido"
+                                :class="{'is-invalid':errors.has('apellido')}"
                                 v-model="usuario.apellido"
+                                v-validate="'required'"
                                 required
                                 placeholder="Ingresa tu apellido">
                     </b-form-input>
+                    <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group label="E-mail:" label-for="email">
                     <b-form-input id="email"
+                                name="email"
                                 v-model="usuario.email"
+                                :class="{'is-invalid':errors.has('email')}"
+                                v-validate="'required|email'"
                                 required
                                 placeholder="Ingresa tu E-mail">
                     </b-form-input>
+                    <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group label="Contraseña:" label-for="password">
                     <b-form-input id="password"
+                                name="password"
+                                ref="password"
+                                :class="{'is-invalid':errors.has('password')}"
+                                v-validate="'required'"
                                 v-model="usuario.password"
                                 required
                                 placeholder="Ingresa tu Contraseña">
                     </b-form-input>
+                    <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group label="Confirmar contraseña:" label-for="password_confirmation">
                     <b-form-input id="password_confirmation"
+                                name="password_confirmation"
+                                :class="{'is-invalid':errors.has('password_confirmation')}"
+                                v-validate="'required|confirmed:password'"
                                 v-model="usuario.password_confirmation"
                                 required
                                 placeholder="Confirma tu Contraseña">
                     </b-form-input>
+                        <b-form-invalid-feedback>Las contraseñas no coinciden</b-form-invalid-feedback>
                 </b-form-group>
                 
                 <!-- SELECT UNIVERSIDADES -->
@@ -48,12 +66,15 @@
                 label-for="universidad">
                 <b-form-select
                     v-model="usuario.universidad" 
+                    :class="{'is-invalid':errors.has('universidad')}"
+                    v-validate="'required'"
                     id="universidad" 
                     :options="optionsUniversidad" 
                     name="universidad" 
                     text-field="description" 
                     value-field="id"
                         />
+                        <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
 
                 <!-- SELECT CARRERAS -->
@@ -61,6 +82,8 @@
                 label="Carrera" 
                 label-for="carrera">
                 <b-form-select
+                    :class="{'is-invalid':errors.has('carrera')}"
+                    v-validate="'required'"
                     v-model="usuario.carrera_id" 
                     id="carrera_id" 
                     :options="optionsCarrera" 
@@ -68,14 +91,25 @@
                     text-field="description" 
                     value-field="id"
                         />
+                <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
                 <b-form-group>
                     <b-form-checkbox-group  id="checkCondiciones">
-                        <b-form-checkbox required value="checkCondiciones">
+                        <b-form-checkbox 
+                                name="checkCondiciones"
+                                :class="{'is-invalid':errors.has('checkCondiciones')}"
+                                v-validate="'required'" 
+                                required 
+                                value="checkCondiciones">
                             Estoy de acuerdo con los <a>terminos y condiciones</a></b-form-checkbox>
                     </b-form-checkbox-group>
+                    <b-form-invalid-feedback>Campor requerdio</b-form-invalid-feedback>
                 </b-form-group>
             </b-form>
+            <b-alert 
+            :show="hasErrors" 
+            variant="danger" 
+            class="text-center">{{ error }}</b-alert>
         </b-container>
         <template slot="modal-footer">
       <button class="btn btn-success btn-block" type="submit"  @click="crearUsuario">Registrarse</button>
@@ -120,18 +154,30 @@ export default {
             disabled: true
         }
         ],
-        urlCarrera:''
+        urlCarrera:'',
+        error: "",
     };
   },
+    computed: {
+        hasErrors() {
+            return this.error != "";
+        }
+    },
   methods :{
         crearUsuario(){
             // console.log(this.usuario);
-            this.axios.post('api/auth/signup/',this.usuario)
+            this.$validator.validateAll().then(result => {
+                if(result){
+                     this.axios.post('api/auth/signup/',this.usuario)
                 .then((response) => {
                     this.$emit("success",this.usuario.email)     
                     this.$refs.newUser.hide();
                     
                 })
+                }else {
+                    this.error = "Por favor, corrija los campos en rojo";
+                }
+            })
         },
         setSuccessMessage(){
             this.console("volvio");
@@ -189,7 +235,9 @@ export default {
             this.usuario.universidad=null,
             this.usuario.carrera_id=null,
             this.usuario.password="",
-            this.usuario.password_confirmation="" 
+            this.usuario.password_confirmation=""
+            this.error="" 
+            this.$validator.reset();
         }
   },
   mounted(){
