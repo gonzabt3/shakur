1b280c703d24e162ec36d8c4a5147f60e2275f30
gonza <gonzalo.muscolo@gmail.com>
2020-09-26 06:45:09 +0000

    add test for delete method

--- tests/Unit/Services/FileServiceTest.php
+++ tests/Unit/Services/FileServiceTest.php
@@ -11,6 +11,8 @@ use App\Publicacion;
 use App\Universidad;
 use App\Carrera;
 use App\Materia;
+use App\File;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Services\FileService;
 use Storage;
 
@@ -32,5 +34,22 @@ class FileServiceTest extends TestCase
         'id' => '1',
         'publicacion_id' => $publicacion->id
       ]);
+  }
+
+  public function testCheckAuthor(){
+    $univerisdad = factory(Universidad::class)->create();
+    $carrera = factory(Carrera::class)->create(['universidad_id' => $univerisdad->id]);
+    $materia = factory(Materia::class)->create(['carrera_id' => $carrera->id]);
+    $user = factory(User::class)->create();
+    $publicacion = factory(Publicacion::class)->create(['materia_id' => $materia->id,
+                                                        'user_id' => $user->id]);
+    $file = factory(File::class)->create(['publicacion_id' => $publicacion->id,
+                                          'documento_id' => null,
+                                          'reporte_id' => null,
+                                          ]);
+    $fileService = new FileService;
+    Auth::shouldReceive('user')->andReturn($user);
+    $checkAuthorFile = $fileService->checkAuthor($file);
+    $this->assertSame(true, $checkAuthorFile);
   }
 }
