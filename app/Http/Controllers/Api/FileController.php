7b55f25f951b95d85060a72423e6a27882e3adb1
gonza <gonzalo.muscolo@gmail.com>
2020-09-26 06:44:43 +0000

    refactor delete file method

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers\Api;
 
 use App\User;
 use App\File;
-
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
@@ -13,72 +12,23 @@ use App\Http\Services\UserService;
 use Session;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File as FacedeFile;  //renombro el facade porque File se llama la clase 
-
-
+use Illuminate\Support\Facades\File as FacadeFile; 
 
 class FileController extends Controller
 {
-    protected $userService;
+  protected $userService;
 
-    public function __construct(UserService $userService){
-        $this->userService = $userService;
+  public function __construct(UserService $userService){
+    $this->userService = $userService;
+  }
+
+  public function delete(Request $request,Response $response,Int $id){
+    $file = File::find($id);
+    if($userService->checkAuth($file)){
+      $path_file= $file->path;
+      $path_final=str_replace('storage/','public/',$path_file);
+      Storage::delete($path_final);
+      File::destroy($id);
     }
-
-    //TODOS ESTOS METODOS HAY QUE PASARLOS AL SERVICE
-
-
-
-        public function delete(Request $request,Response $response,Int $id){
-            //corroboro que sea el autor el que lo esta borrando
-            if(Auth::user()->id==File::find($id)->user_id){
-                $path_file= File::find($id)->path;
-
-                $path_final=str_replace('storage/','public/',$path_file);
-
-                Storage::delete($path_final);
-
-                File::destroy($id);
-            }
-        }
-
-    //FUNCION PARA AGREGAR DOCUMENTACION
-    // public function store(Request $request){
-
-    //     $parameters = $this->validate($request,[
-    //         'nombre' => 'required',
-    //         'idMateria' => 'required',
-    //     ]);
-
-    //     $file =$request->file('file');
-
-    //     $path =public_path().'/storage/app/public/files/'.$parameters['idMateria'];
-
-    //     //si no existe el directorio lo creo
-    //     // if(!FacedeFile::exists($path)){
-    //     //     FacedeFile::makeDirectory($path,$mode = 0777, true, true);
-    //     // }
-            
-    //     //agrego user
-    //     $user = Auth::user();
-    //     $parameters['user_id']=$user->id;
-        
-    //     //ruta del archivo a partir de public
-    //     $pathFile = '/public/files/'.$parameters['idMateria'];
-        
-    //     // AL FACADE SE LE PASE /PUBLIC PARA QUE LO GUARDE EN STORAGE/'PUBLIC'
-    //     // PERO A LA BASE SE LE PASA STORAGE PORQUE CUANDO SE LE VA A BUSCAR SE VA A BUSCAR A LA CARPETA LOCALHOST/STORAGE 
-    //     // QUE ESTA LINKEADA CON PUBLIC MENCIONADA ANTERIORMENTE
-
-
-    //     //guardo
-    //     $fileName=$file->getClientOriginalName(); 
-    //     Storage::putFileAs($pathFile,$file,$fileName);  
-
-    //     $parameters['path'] = 'storage/files/'.$parameters['idMateria'].'/'.$file->getClientOriginalName();
-    //     $parameters['materia_id']=$parameters['idMateria'];
-
-    //     $fileObject = File::create($parameters);
-    //     $fileObject->save();
-    //     }
+  }
 }
--- app/Http/Services/FileService.php
+++ app/Http/Services/FileService.php
@@ -17,97 +17,99 @@ use App\User;
 use Session;
 
 class FileService {
-
-    public function store($file,$id,$tipo){
-        //el tipo puede ser POST O DOCUMENTO
-        if($tipo=='post'){
-            $idPost=$id;
-            $idDocumento=null;
-            $idReport=null;
-        }
-
-        if($tipo=='file'){
-            $idPost=null;
-            $idDocumento=$id;
-            $idReport=null;
-        }
-
-        if($tipo=='report'){
-            $idPost=null;
-            $idDocumento=null;
-            $idReport=$id;
-        }
-
-        DB::beginTransaction();
-        try {
-
-            $fileObject['publicacion_id']=$idPost;  
-            $fileObject['documento_id']=$idDocumento;
-            $fileObject['reporte_id']=$idReport;
-            $fileObject['path']='';
-            $fileObject['nombre']=$file->getClientOriginalName();
-            $fileObject['extension']=substr($file->getClientOriginalName(), -3);
-            
-            $idFile = File::create($fileObject);
-
-            $currentFile=File::find($idFile->id);
-            $currentFile->path='storage/files/'.$idFile->id.'/'.$file->getClientOriginalName();
-            $currentFile->save();
-           
-            $this->toDisk($file,$idFile->id);
-        
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollback();
-        }
-    }   
-
-
-    public function delete($id,$tipo=null){
-        $file=File::find($id);
-        
-        // el tipo siver para saber desde donde vine el borrado
-        if($tipo=='documento'){
-            //me fijo si no tiene una publicacion asociado y lo borro SINO lo des asocio
-            if($file->publicacion_id==null){
-
-                $path_final=str_replace('storage/','public/',$file->path);
-
-                Storage::delete($path_final);
-
-                 File::destroy($id);
-            }else{
-                 File::where('id', $id)->update(array('documento_id' => null));
-            }
-        }
-       
-        if($tipo=='post'){
-            //me fijo si no tiene un doc asociado y lo borro SINO lo des asocio
-            if($file->documento_id==null){
-
-                $path_final=str_replace('storage/','public/',$file->path);
-
-                Storage::delete($path_final);+
-                File::destroy($id);
-            }else{
-                 File::where('id', $id)->update(array('publicacion_id' => null));
-            }
-        }
+  public function store($file,$id,$tipo){
+    //el tipo puede ser POST O DOCUMENTO
+    if($tipo=='post'){
+      $idPost=$id;
+      $idDocumento=null;
+      $idReport=null;
     }
 
-    public function toDisk($file,$idFolder){
-            
-        //ruta del archivo a partir de public
-        $pathFile = '/public/files/'.$idFolder;
-        
-        // AL FACADE SE LE PASE /PUBLIC PARA QUE LO GUARDE EN STORAGE/'PUBLIC'
-        // PERO A LA BASE SE LE PASA STORAGE PORQUE CUANDO SE LE VA A BUSCAR SE VA A BUSCAR A LA CARPETA LOCALHOST/STORAGE 
-        // QUE ESTA LINKEADA CON PUBLIC MENCIONADA ANTERIORMENTE
-
-        //guardo
-        $fileName=$file->getClientOriginalName(); 
-        // dd($pathFile);
-        Storage::putFileAs($pathFile,$file,$fileName);  
-
+    if($tipo=='file'){
+      $idPost=null;
+      $idDocumento=$id;
+      $idReport=null;
     }
+
+    if($tipo=='report'){
+      $idPost=null;
+      $idDocumento=null;
+      $idReport=$id;
+    }
+
+    DB::beginTransaction();
+    try {
+      $fileObject['publicacion_id']=$idPost;  
+      $fileObject['documento_id']=$idDocumento;
+      $fileObject['reporte_id']=$idReport;
+      $fileObject['path']='';
+      $fileObject['nombre']=$file->getClientOriginalName();
+      $fileObject['extension']=substr($file->getClientOriginalName(), -3);
+      $idFile = File::create($fileObject);
+      $currentFile=File::find($idFile->id);
+      $currentFile->path='storage/files/'.$idFile->id.'/'.$file->getClientOriginalName();
+      $currentFile->save();      
+      $this->toDisk($file,$idFile->id);
+      DB::commit();
+    } catch (\Exception $e) {
+      DB::rollback();
+    }
+  }   
+
+
+  public function delete($id,$tipo=null){
+    $file=File::find($id);
+    
+    // el tipo siver para saber desde donde vine el borrado
+    if($tipo=='documento'){
+      //me fijo si no tiene una publicacion asociado y lo borro SINO lo des asocio
+      if($file->publicacion_id==null){
+        $path_final=str_replace('storage/','public/',$file->path);
+        Storage::delete($path_final);
+        File::destroy($id);
+      }else{
+        File::where('id', $id)->update(array('documento_id' => null));
+      }
+    }
+    
+    if($tipo=='post'){
+      //me fijo si no tiene un doc asociado y lo borro SINO lo des asocio
+      if($file->documento_id==null){
+        $path_final=str_replace('storage/','public/',$file->path);
+        Storage::delete($path_final);+
+        File::destroy($id);
+      }else{
+          File::where('id', $id)->update(array('publicacion_id' => null));
+      }
+    }
+  }
+
+  public function toDisk($file,$idFolder){
+    //ruta del archivo a partir de public
+    $pathFile = '/public/files/'.$idFolder;
+    
+    // AL FACADE SE LE PASE /PUBLIC PARA QUE LO GUARDE EN STORAGE/'PUBLIC'
+    // PERO A LA BASE SE LE PASA STORAGE PORQUE CUANDO SE LE VA A BUSCAR SE VA A BUSCAR A LA CARPETA LOCALHOST/STORAGE 
+    // QUE ESTA LINKEADA CON PUBLIC MENCIONADA ANTERIORMENTE
+
+    //guardo
+    $fileName=$file->getClientOriginalName(); 
+    Storage::putFileAs($pathFile,$file,$fileName);
+  }
+
+  public function checkAuthor(File $file){
+    return Auth::user()->id==$this->getIdUserFile($file);
+  }
+
+  private function getIdUserFile(File $file){
+    if($file->publicacion_id != null){
+      return  $file->publicacion->user_id;
+    }
+    if($file->documento_id != null){
+      return  $file->documento->user_id;
+    }
+    if($file->reporte_id != null){
+      return  $file->reportError->user_id;
+    }
+  }
 }
